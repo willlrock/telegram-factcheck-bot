3dc74ab67deb82c4815c1c80ba76443b1b48c137
Xurshid Muhammadiyev <xurshidmuhammadiyev@gmail.com>
2026-07-01 16:46:11 +0500

    add referer to downloader for instagram

--- app/services/downloader.py
+++ app/services/downloader.py
@@ -31,6 +31,7 @@ def download_video_audio(url: str) -> str:
         'no_warnings': True,
         'nocheckcertificate': True,
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'referer': 'https://www.instagram.com/',
     }
     
     try:
